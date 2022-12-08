7b71eb95edbc10d7a85ae40b9741212b33dee44e
Mukhammadliti <mukhammadaliti02@gmail.com>
2022-12-08 15:27:36 +0600

    calculator

---
+++ lib/buttons.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Buttons extends StatelessWidget {
+  const Buttons(
+      {super.key, this.color, this.textColor, required this.buttonText});
+  final color;
+  final textColor;
+  final String buttonText;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            color: color,
+            child: Center(
+                child: Text(
+              buttonText,
+              style: TextStyle(color: textColor),
+            )),
+          )),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:calculator_app/buttons.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> buttons = [
+    'C',
+    'DEL',
+    '%',
+    '/',
+    '9',
+    '8',
+    '7',
+    'x',
+    '6',
+    '5',
+    '4',
+    '-',
+    '3',
+    '2',
+    '1',
+    '+',
+    '0',
+    '.',
+    'ANS',
+    '=',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              child: GridView.builder(
+                itemCount: buttons.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4),
+                itemBuilder: (BuildContext context, int index) {
+                  return Buttons(
+                    buttonText: buttons[index],
+                  color: Colors.deepPurple,
+                  textColor: Colors.white,
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  bool isOperator (String x){
+    if (x == '%' || x == '/' || x == 'x' || x == '-' || x == '+' || x == '='){
+      return true;
+    }
+    return false;
+  }
+}
